9328cf616efb0f6492b747da5dcc883fa78ac65b
Kevin <kevhines@gmail.com>
2021-04-23 14:17:09 -0400

    commit tree 7613c3574840d0c0d06bb73f57be1c1bfc281df6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, f) {
+    const newArray = []
+    array.forEach( element => {
+        newArray.push(f(element))
+    })
+    return newArray
+}
+
+function reduce(array,f,start) {
+    let result = (!!start) ? start : array[0]
+    // if (start) {result = start; }
+    array.forEach( element => {
+        if (!start && element === array[0]) {} else {result = (f(element, result))}
+    })
+    return result
+
+}
